93110b21cb1b526c255b5188a57bed0d53c7bd11
tukiyo3 <tukiyo3@gmail.com>
2019-01-04 07:39:52 +0900

    fix: clonezilla

--- clonezilla/extract_clonezilla_amd64.sh
+++ clonezilla/extract_clonezilla_amd64.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CLONEZILLA_URL="http://iij.dl.osdn.jp/clonezilla/69912/clonezilla-live-2.5.6-22-amd64.zip"
+CLONEZILLA_URL="https://jaist.dl.osdn.jp/clonezilla/69909/clonezilla-live-20180812-bionic-amd64.zip"
 ls clonezilla-live-*.zip || wget $CLONEZILLA_URL
 
 unzip -j clonezilla-live-*.zip \
